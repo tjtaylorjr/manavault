5fa4177c60f24709dc724048a2e2f1ce5d06ebe9
TJ Taylor <tjtaylorjr@gmail.com>
2021-01-10 16:03:54 -0500

    added splash page

--- mana-vault/src/App.js
+++ mana-vault/src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <>
+    <div className="splash">
+      <h1 className="title">ManaVault</h1>
+      <h2 className="splash-statement">Coming Soon</h2>
+      <p className="disclaimer">This site is unofficial Fan Content permitted under the Fan Content Policy. Not approved/endorsed by Wizards. Portions of the materials used are property of Wizards of the Coast. ©Wizards of the Coast LLC.</p>
+    </div>
+    </>
   );
 }
 

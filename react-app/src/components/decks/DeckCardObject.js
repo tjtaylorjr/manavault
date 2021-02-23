7a176a4843b81100563b5846b7b5139da4c7a96f
TJ Taylor <tjtaylorjr@gmail.com>
2021-02-22 23:33:54 -0500

    cleaned up code on deckcardobject.js by removing unneeded old code

--- react-app/src/components/decks/DeckCardObject.js
+++ react-app/src/components/decks/DeckCardObject.js
@@ -1,71 +1,3 @@
-// import React, { useState, useEffect } from 'react'
-// import useAsync from "../../utils/helpers.js";
-
-// const DeckCardObject = (data) => {
-//   const [smallIMG, setSmallIMG] = useState("");
-//   // const [normalIMG, setNormalIMG] = useState("");
-//   const [fullCard, setFullCard] = useState({});
-//   // const [pictures, setPictures] = useState({"fake": "object"})
-//   const [correctName, setCorrectName] = useState("");
-//   const [mounted, setMounted] = useState(false);
-
-//   // const {card_id} = card.card;
-
-
-//   useEffect(() => {
-//     setMounted(false);
-//     (async () => {
-//       const {card_id} = data.data;
-
-//       const res = await fetch(`/api/cards/${card_id}`, {
-//         headers: {
-//           "Content-Type": "application/json",
-//         }
-//       });
-
-//       if(!res.ok) {
-//         throw res
-//       }
-
-//       const result = await res.json();
-//       if(result.length > 0) {
-//         console.log(result.result)
-//         setFullCard(result);
-//         console.log(fullCard)
-//         setMounted(true);
-//       }
-//     })()
-
-//   }, [data.data, fullCard])
-
-//   useEffect(() => {
-//     if (mounted && fullCard.length > 0) {
-//       const { illustration } = fullCard;
-//       console.log(fullCard)
-//       const { small_image } = illustration;
-//       setSmallIMG(small_image);
-//       setCorrectName(fullCard.name.split(" //"));
-//     }
-
-//   },[mounted])
-
-//   // const renderCardPage = () => {
-//   //   alert('Card information modal coming soon!');
-//   // }
-
-//   return (
-//     <section className="deckcard-object">
-//       <h4 style={{ color: "#FFF" }}>{correctName}</h4>
-//       <div className="deckcard-object__image-container">
-//           <img src={smallIMG} alt={correctName + " card image"} />
-//       </div>
-//     </section>
-//   )
-// }
-
-// export default DeckCardObject;
-
-
 import React, { useState, useEffect } from 'react'
 
 const DeckCardObject = (props) => {
@@ -73,10 +5,6 @@ const DeckCardObject = (props) => {
   const [normalIMG, setNormalIMG] = useState("")
   const { card} = props.data;
   const { showImagePreview, dropImagePreview} = props;
-  // const numCopies = props.num;
-  // console.log(showImagePreview);
-  // console.log(card)
-  //const formatted_name = card.name.split(" //")
 
   useEffect(() => {
     let mounted = true;

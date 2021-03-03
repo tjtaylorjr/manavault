3f6b311a1f21187b54d4b0f25781656b3a7a38a6
TJ Taylor <tjtaylorjr@gmail.com>
2021-03-02 21:39:58 -0500

    cleanup pass on BuildSearchCardObject.js

--- react-app/src/components/cards/BuildSearchCardObject.js
+++ react-app/src/components/cards/BuildSearchCardObject.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ItemTypes } from '../../utils/helpers';
 import { useDrag } from 'react-dnd';
-import { FaSatelliteDish } from 'react-icons/fa';
 
 const BuildSearchCardObject = ( props ) => {
   const [normalIMG, setNormalIMG] = useState("");
@@ -29,9 +28,9 @@ const BuildSearchCardObject = ( props ) => {
     return () => mounted = false;
   },[props.card, data.illustration.normal_image])
 
-  const renderCardPage = () => {
-    alert('Card information modal coming soon!');
-  }
+  // const renderCardPage = () => {
+  //   alert('Card information modal coming soon!');
+  // }
 
 
 
@@ -44,11 +43,11 @@ const BuildSearchCardObject = ( props ) => {
 
 
   return (
-    <section className="build-search-card-object card" onClick={renderCardPage}
+    <section className="build-search-card-object card"
+      // onClick={renderCardPage}
       ref={dragRef}
       style={{opacity: isDragging ? .5 : 1, cursor: 'grab',}}
     >
-      {/* <h4 style={{ color:"#FFF" }}>{formatted_name[0]}</h4> */}
       <div className="build-search-card-object__image-container">
         {normalIMG ? (
           <img src={normalIMG} id={ID} name={name} alt={data.name + " card image"} cmc={data.conv_mana_cost} onDragStart={dragStart} onDrop={cardDrop} onMouseEnter={showImagePreview} onMouseLeave={dropImagePreview}/>

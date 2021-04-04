403cefcbd4309d8ab910dfb9356ae3c4e57a264b
TJ Taylor <tjtaylorjr@gmail.com>
2021-04-03 22:06:31 -0400

    added more comments to seed file

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -14,6 +14,26 @@ def seed_comments():
                        deck_id=1,
                        content="Gruul clan best clan")
 
+    comment4 = Comment(user_id=7,
+                       deck_id=2,
+                       content="NGL Can be super annoying to play against")
+
+    comment5 = Comment(user_id=11,
+                       deck_id=3,
+                       content="I like the lifegain version with the broodmoths and daxos")
+
+    comment6 = Comment(user_id=15,
+                       deck_id=3,
+                       content="Sometimes it is fun to just turn cards sideways")
+
+    comment7 = Comment(user_id=18,
+                       deck_id=3,
+                       content="Looks pretty good")
+
+    comment8 = Comment(user_id=19,
+                       deck_id=1,
+                       content="Not a fan, personally")
+
     new_user = User(
                  username="icordova",
                  email="icordova@email.com",
@@ -25,7 +45,9 @@ def seed_comments():
                                 user_id=27,
                                 location="Sioux City, IA")
 
-    db.session.add_all([comment1, comment2, comment3, new_user, user_profile27])
+    db.session.add_all([comment1, comment2, comment3, comment4,
+                        comment5, comment6, comment7, comment8,
+                        new_user, user_profile27])
     db.session.commit()
 
     # print('---COMMENTS---', comment1.to_dict())
